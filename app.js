8ace236d0888ea4fa7910d1db2bd69d3dd8aef33
unknown <kartikthakur5026@gmail.com>
2023-06-24 14:41:05 +0530

    add files

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.post("/",(req,res)=>{
 app.get("/failure",(req,res)=>{
     res.redirect("/");
 })
-app.listen(process.env.PORT || 3000,()=>{
+app.listen(3000,()=>{
     console.log("server is running on port 3000");
 })
 
